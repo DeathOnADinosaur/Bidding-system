a91e7735316f73c9944cc5c8e7e20579ffda48b8
Luke Melton <deathonadinosaur@gmail.com>
2018-12-02 16:34:45 -0800

    halp

--- Tut 1 - Client/Tut 1 - Client/Source.cpp
+++ Tut 1 - Client/Tut 1 - Client/Source.cpp
@@ -4,20 +4,33 @@ Date: 11/13/2015
 This is the solution for the client that you should have at the end of tutorial 1.
 */
 #pragma comment(lib,"ws2_32.lib")
+
 #include <WinSock2.h>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <ctime>
+
 using namespace std;
 struct item {
-	char name;
-	double price;
-	double amount;
+	string name;
+	int price;
+	int amount;
 };
 
-unsigned char * deserialize_int(unsigned char *buffer, int *value);
+item items[4];
+
+struct client {
+	string name;
+	int bid;
+	string item;
+	int maxBid;
+};
+
+int max = rand() % 100 + 1;
+
 int main()
 {
-	
 	//Winsock Startup
 	WSAData wsaData;
 	WORD DllVersion = MAKEWORD(2, 1);
@@ -43,7 +56,50 @@ int main()
 
 	char MOTD[256];
 	recv(Connection, MOTD, sizeof(MOTD), NULL); //Receive Message of the Day buffer into MOTD array
-	std::cout << "MOTD:" << MOTD << std::endl;
+
+	string tmp,word;
+	tmp = MOTD;
+	stringstream iss(tmp);
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 3; j++) {
+			iss >> word;
+			if (j == 0)
+				items[i].name = word;
+
+			if (j == 1)
+				items[i].price = stoi(word);
+
+			if (j == 2)
+				items[i].amount = stoi(word);
+		}
+	}
+
+	cout << endl;
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "Name: " << items[i].name << endl;
+		cout << "Price: " << items[i].price << endl;
+		cout << "Amount: " << items[i].amount << endl;
+	}
+	cout << endl;
+
+	client me;
+	me.name = "frank";
+	srand(0);
+	int randomItem = rand() % 3;
+	
+	me.item = items[randomItem].name;
+	me.maxBid = max;
+
+	if(me.maxBid >= items[randomItem].price + 1)
+		me.bid = items[randomItem].price + 1;
+
+	string sndTmp = me.name + "\n" + to_string(me.bid) + "\n" + me.item;
+	const char* msg = sndTmp.c_str();
+	send(Connection, msg, 255, NULL); 
+
+	//std::cout << "MOTD:" << MOTD << std::endl;
 	while (true)
 	{
 		Sleep(10);
